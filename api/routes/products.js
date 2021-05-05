4253c9e7452b216d47a8357face70d3131e8759a
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2021-05-06 01:06:20 +0200

    Parse and return body request

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,9 +7,14 @@ router.get('/', (req, res) => {
   })
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
   res.status(201).json({
-    message: 'Product was created'
+    message: 'Product was created',
+    createdProduct: product // returns the information we sent to the server
   })
 })
 
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
+app.use(express.json()) // this is to parse the body as json
+
 app.use('/products', productRoutes) // it forwards all the /products requests to localhost:3000/products
 app.use('/orders', orderRoutes) // it forwards all the /orders requests to localhost:3000/orders
 
